b7af591b2b9e0385c834ded09ccc5bf988f82263
root2raj <ruturaj1999@gmail.com>
2018-12-26 21:38:02 +0000

    Add Snap camera

--- device.mk
+++ device.mk
@@ -310,6 +310,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-service \
+    libxml2 \
+    Snap
+
 # Wfd
 PRODUCT_BOOT_JARS += \
     WfdCommon
