1dba2e575548f7af4003942ebb0989cbd0ce1c0a
root2raj <ruturaj1999@gmail.com>
2018-12-29 18:04:58 +0530

    Add maintainer prop

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -268,3 +268,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=root2raj
